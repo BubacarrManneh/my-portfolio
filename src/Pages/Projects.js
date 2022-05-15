b97e7360988d2a4b49b658d6e1444522341e2ed9
BubacarrManneh <bubacarr.manneh@integrify.io>
2022-05-15 20:08:18 +0200

    Optimising page

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -14,21 +14,21 @@ const Projects = () => {
           <p>
            <a href='http://bubacarrm.netlify.app' target='_blank' rel="noreferrer" ><button className='bg-sky-500 p-3 rounded-lg'>Portfolio</button></a>
           </p>
-          <iframe height='250' width='400' className='rounded-2xl mb-6 pt-3' title='portfolio' src='http://bubacarrm.netlify.app'></iframe>
+          <iframe height='200' width='300' className='rounded-2xl mb-6 pt-3' title='portfolio' src='http://bubacarrm.netlify.app'></iframe>
         </div>
         <div>
           <p>
            <a href='http://bubacarrtodo.netlify.app' target='_blank' rel="noreferrer" ><button className='bg-sky-500 p-3 rounded-lg'>Todo App</button></a>
           </p>
-          <iframe height='250' width='400' className='rounded-2xl mb-6 pt-3' title='portfolio' src='http://bubacarrtodo.netlify.app'></iframe>
+          <iframe height='2oo' width='300' className='rounded-2xl mb-6 pt-3' title='portfolio' src='http://bubacarrtodo.netlify.app'></iframe>
         </div>
         <div>
           <p><a href='http://albuba.netlify.app' target='_blank' rel="noreferrer" ><button className='bg-sky-500 p-3 rounded-lg'>My website</button></a></p>
-           <iframe height='250' width='400' className='rounded-2xl mb-6 pt-3' title='portfolio' src='http://albuba.netlify.app'></iframe>
+           <iframe height='200' width='300' className='rounded-2xl mb-6 pt-3' title='portfolio' src='http://albuba.netlify.app'></iframe>
         </div>
         <div>
           <p><a href='http://abuweather.netlify.app' target='_blank' rel="noreferrer" ><button className='bg-sky-500 p-3 rounded-lg'>Weather App</button></a></p>
-          <iframe height='250' width='400' className='rounded-2xl mb-6 pt-3' title='portfolio' src='http://abuweather.netlify.app'></iframe>
+          <iframe height='200' width='300' className='rounded-2xl mb-6 pt-3' title='portfolio' src='http://abuweather.netlify.app'></iframe>
         </div>    
       </div>
     </div>
