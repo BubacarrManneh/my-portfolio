fe7eafcfe2ede90ee6ba28b454a423aee89deba8
BubacarrManneh <bubacarr.manneh@integrify.io>
2022-05-19 17:18:54 +0200

    Update website

--- README.md
+++ README.md
@@ -2,7 +2,5 @@
 ## Building Portfolio website using react and tailwindcss
 ## Add SVG icons for work, location and social media
 ## Create a contact me form using google forms
-<<<<<<< HEAD
-=======
 
->>>>>>> 6a9cc44932c0ef44c7fe5abec5a894a77f35a1ae
+
--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -3,36 +3,139 @@ import Navbar from "../Components/Navbar"
 import background from "../Images/background.jpg";
 
 const Projects = () => {
+
+    const portfolio = `http://bubacarrm.netlify.app`;
+    const todoApp = `http://bubacarrtodo.netlify.app`;
+    const mySite = `http://albuba.netlify.app`;
+    const weatherApp = `http://abuweather.netlify.app`;
+    // const restCoutries = `Rest Countries`;
+
+ 
+
+  const handleClick = (url) => {
+    window.open(url);
+  };
+
   return (
-    <div  className="text-gray-100 leading-normal tracking-wider bg-cover lg:mb-0"
-        style={{ backgroundImage: `url(${background})` }}>
+    <div
+      className="text-gray-100 leading-normal tracking-wider bg-cover lg:mb-0 h-100%"
+      style={{ backgroundImage: `url(${background})` }}
+    >
       <div className="bg-steal-900 bg-emerald-700">
         <Navbar />
       </div>
-      <div className='justify-around flex flex-wrap pt-6'>
-        <div>
-          <p>
-           <a href='http://bubacarrm.netlify.app' target='_blank' rel="noreferrer" ><button className='bg-sky-500 p-3 rounded-lg'>Portfolio</button></a>
-          </p>
-          <iframe height='200' width='300' className='rounded-2xl mb-6 pt-3' title='portfolio' src='http://bubacarrm.netlify.app'></iframe>
+      <div className="justify-around flex flex-wrap pt-6">
+        <div className=" bg-teal-700 pb-4 mb-2 rounded-2xl rounded-br-2xl">
+          <iframe
+            height="200"
+            width="400"
+            scrolling="no"
+            className="rounded-2xl mb-6  cursor-pointer"
+            title="WeatherApp"
+            target="_blank"
+            rel="noreferrer"
+            src={portfolio}
+          ></iframe>
+          <button
+            onClick={handleClick}
+            className="bg-sky-900 hover:bg-sky-700 font-bold p-3 rounded-lg"
+          >
+            Portfolio
+          </button>
         </div>
-        <div>
-          <p>
-           <a href='http://bubacarrtodo.netlify.app' target='_blank' rel="noreferrer" ><button className='bg-sky-500 p-3 rounded-lg'>Todo App</button></a>
-          </p>
-          <iframe height='2oo' width='300' className='rounded-2xl mb-6 pt-3' title='portfolio' src='http://bubacarrtodo.netlify.app'></iframe>
+        <div className=" bg-teal-700 pb-4 mb-2 rounded-2xl rounded-br-2xl">
+          <iframe
+            height="200"
+            width="400"
+            scrolling="no"
+            className="rounded-2xl mb-6 cursor-pointer"
+            title="WeatherApp"
+            target="_blank"
+            rel="noreferrer"
+            src={todoApp}
+          ></iframe>
+          <button
+            onClick={handleClick}
+            className="bg-sky-900 hover:bg-sky-700 font-bold p-3 rounded-lg"
+          >
+            Todo App
+          </button>
         </div>
-        <div>
-          <p><a href='http://albuba.netlify.app' target='_blank' rel="noreferrer" ><button className='bg-sky-500 p-3 rounded-lg'>My website</button></a></p>
-           <iframe height='200' width='300' className='rounded-2xl mb-6 pt-3' title='portfolio' src='http://albuba.netlify.app'></iframe>
+        <div className=" bg-teal-700 pb-4 mb-2 rounded-2xl rounded-br-2xl">
+          <iframe
+            height="200"
+            width="400"
+            scrolling="no"
+            className="rounded-2xl mb-6  cursor-pointer"
+            title="WeatherApp"
+            target="_blank"
+            rel="noreferrer"
+            src={weatherApp}
+          ></iframe>
+          <button
+            onClick={handleClick}
+            className="bg-sky-900 hover:bg-sky-700 font-bold p-3 rounded-lg"
+          >
+            WeatherApp
+          </button>
         </div>
-        <div>
-          <p><a href='http://abuweather.netlify.app' target='_blank' rel="noreferrer" ><button className='bg-sky-500 p-3 rounded-lg'>Weather App</button></a></p>
-          <iframe height='200' width='300' className='rounded-2xl mb-6 pt-3' title='portfolio' src='http://abuweather.netlify.app'></iframe>
-        </div>    
+        <div className=" bg-teal-700 pb-4 mb-2 rounded-2xl rounded-br-2xl">
+          <iframe
+            height="200"
+            width="400"
+            scrolling="no"
+            className="rounded-2xl mb-6  cursor-pointer"
+            title="WeatherApp"
+            target="_blank"
+            rel="noreferrer"
+            src={mySite}
+          ></iframe>
+          <button
+            onClick={handleClick}
+            className="bg-sky-900 hover:bg-sky-700 font-bold p-3 rounded-lg"
+          >
+            My Website
+          </button>
+        </div>
+        <div className=" bg-teal-700 pb-4 mb-2 rounded-2xl rounded-br-2xl">
+          <iframe
+            height="200"
+            width="400"
+            scrolling="no"
+            className="rounded-2xl mb-6  cursor-pointer"
+            title="WeatherApp"
+            target="_blank"
+            rel="noreferrer"
+            src={weatherApp}
+          ></iframe>
+          <button
+            onClick={handleClick}
+            className="bg-sky-900 hover:bg-sky-700 font-bold p-3 rounded-lg"
+          >
+            WeatherApp
+          </button>
+        </div>
+        <div className=" bg-teal-700 pb-4 mb-2 rounded-2xl rounded-br-2xl">
+          <iframe
+            height="200"
+            width="400"
+            scrolling="no"
+            className="rounded-2xl mb-6  cursor-pointer"
+            title="WeatherApp"
+            target="_blank"
+            rel="noreferrer"
+            src={weatherApp}
+          ></iframe>
+          <button
+            onClick={handleClick}
+            className="bg-sky-900 hover:bg-sky-700 font-bold p-3 rounded-lg"
+          >
+            WeatherApp
+          </button>
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default Projects
